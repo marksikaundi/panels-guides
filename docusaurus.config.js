6df285f81f1c893e0b48bae3995f4e8fc60f2725
Dani Garcia <fillito@gmail.com>
2024-04-17 11:47:25 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/Produkt/panels-guides/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/Produkt/panels-guides/",
         },
         blog: false,
         theme: {
